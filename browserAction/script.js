86c9baff5638155d16c57fa7adcf7f7d1f8ebba2
chee <chee@snoot.club>
2019-01-05 10:30:18 +0000

    use codemirror for javascripting

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,35 +1,51 @@
-let javascript = document.getElementById('javascript')
-let defaultCode =
-`// enter some
-// javascript here
-// and it will
-// run on every page
-// on this domain (location.host)`
+
+let javascript = document.getElementById("javascript")
+
+let editor = CodeMirror.fromTextArea(javascript, {
+	lineNumbers: true,
+	mode: "application/javascript",
+	matchBrackets: true,
+	tabSize: 2,
+	indentWithTabs: true,
+	lint: CodeMirror.lint.javascript,
+	theme: "gruvbox-dark"
+})
+
+editor.setOption("lint", {
+	options: {
+		esversion: 6
+	}
+})
+
+let defaultCode = `\
+// enter some javascript here and it will run
+// on every page on this domain (location.host)
+`
 
 async function getActiveTab () {
-  let tabs = await browser.tabs.query({
-    active: true,
-    currentWindow: true
-  })
+	let tabs = await browser.tabs.query({
+		active: true,
+		currentWindow: true
+	})
 
-  return tabs[0]
+	return tabs[0]
 }
 
 async function getHost () {
-  let {url} = await getActiveTab()
-  let {host} = new URL(url)
-  return host
+	let {url} = await getActiveTab()
+	let {host} = new URL(url)
+	return host
 }
 
-getHost().then(async host => {
-  let store = await browser.storage.local.get()
-  let code = store[await getHost()] || defaultCode
-  javascript.value = code
+getHost().then(async (host) => {
+	let store = await browser.storage.local.get()
+	let code = store[await getHost()] || defaultCode
+	editor.setValue(code)
 })
 
-javascript.addEventListener('input', async event => {
-  let host = await getHost()
-  browser.storage.local.set({
-    [host]: javascript.value
-  })
+editor.on("change", async (editor) => {
+	let host = await getHost()
+	browser.storage.local.set({
+		[host]: editor.getValue()
+	})
 })
