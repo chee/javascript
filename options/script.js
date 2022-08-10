7d47599ff6e3078df3273fb880089e3324dab4fd
chee <yay@chee.party>
2022-08-10 16:43:33 -0500

    add primitive import/export

--- options/script.js
+++ options/script.js
@@ -1,1 +1,43 @@
-document.getElementById('myHeading').style.color = 'red'
+/**
+	@type {HTMLTextAreaElement}
+*/
+let textarea = document.getElementById("textarea")
+let saveButton =document.getElementById("save-button")
+let parseErrorElement = document.getElementById("parse-error")
+let doneMessageElement = document.getElementById("done-message")
+
+let editor = CodeMirror.fromTextArea(textarea, {
+	lineNumbers: true,
+	mode: "application/json",
+	matchBrackets: true,
+	tabSize: 2,
+	indentWithTabs: true,
+	lint: CodeMirror.lint.json,
+	theme: "cobalt"
+})
+
+browser.storage.local.get().then(storage => {
+	 editor.setValue(JSON.stringify(storage, null, "\t"))
+})
+
+function hide(element) {
+	element.setAttribute("hidden", "hidden")
+}
+
+function show(element) {
+	element.removeAttribute("hidden")
+}
+
+
+saveButton.addEventListener("click", async () => {
+	hide(doneMessageElement)
+	hide(parseErrorElement)
+	try {
+		let config = JSON.parse(editor.getValue())
+		await browser.storage.local.clear()
+		await browser.storage.local.set(config)
+      show(doneMessageElement)
+	} catch (error) {
+      show("hidden")
+	}
+})
